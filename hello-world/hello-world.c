4b0d81fcd031c00b7910d1d2c1c6c91ee3c615aa
yudaitateishi <b133736@hiroshima-u.ac.jp>
2017-10-25 15:47:05 +0900

    var

--- hello-world/hello-world.c
+++ hello-world/hello-world.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+
+/*printf()の練習*/
+/*%d,データで代入表示できる*/
 main() 
 {
-	printf("Hello,World\n");
+	printf("Hello,World!\n");
+	printf("Hello,\nWorld!\n");
+	printf("%dは%dより%d大きい。\n",3,2,3-2);
 }
---
+++ hello-world/var.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*変数宣言の練習*/
+
+main()
+{
+	int a=2,b=3;
+	printf("%d-%d=%d\n",b,a,b-a);
+}
